2ebd140711717be3b54e61bd49457c4b1ed7bb15
root <root@605cd65d25a2>
2023-01-20 02:26:20 +0000

    complete module after bullseye

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -9,15 +9,47 @@ public class WalkOfFame {
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+rob.moveTo(50, 300);
 		// 2. Make the robot draw a star shape. Hint: angle=144.
 
-		// 3. Set the length of each line in the star to 30.
+rob.setSpeed(60);
+
+
+for( int i=0; i<10; i++)
+	{
+rob.setAngle(90);
+rob.move(80);
+	rob.penDown();
+	
+	rob.move(30);
+rob.turn(144);
+
+rob.move(30);
+rob.turn(144);
+rob.penDown();
+rob.move(30);
+rob.turn(144);
+
+rob.move(30);
+rob.turn(144);
+
+rob.move(30);
+rob.penUp();	
+	}
+
+rob.move(300);
+
+
+
+
+
+// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of 10 stars (see recipe to see how it should look)
+	
 		 * 
+		 Make the robot draw a line of 10 stars (see recipe to see how it should look)
 		 * Hint: The distance between stars is 50.
 		 */
 
--- src/_03_remainder/_2_bullseye/bullseye/bullseye.pde
+++ src/_03_remainder/_2_bullseye/bullseye/bullseye.pde
@@ -2,13 +2,27 @@
 void setup() {
  
   // set the size of your sketch
-  
+  size(600,600);
 }
 
 void draw() {
   
   //Starting with the largest ellipse, use a for loop to draw a bullseye with ellipses.
+
   
+  for(int i=300;i>50;i-=30)
+ {
+ 
+   if(i%20==0)
+   {
+    fill(300,0,0);
+   }
+   else
+   {
+    fill(0,0,0); 
+   }  
+   ellipse(300,300,i,i);
+}
   //Use an if statement and remainder to alternate the color of your rings.
   
   
